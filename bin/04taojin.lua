73b7589012712b06e9373f041bcc929dd89a6f36
XiNGRZ <chenxingyu92@gmail.com>
2016-03-23 00:01:08 +0800

    debug

--- bin/04taojin.lua
+++ bin/04taojin.lua
@@ -4,13 +4,25 @@ local event = require("event")
 
 local signal = require("signal")
 
-local addresses = require("mapping").load("/mtcs/mapping/04taojin.txt")
+local addresses = require("mapping").load("/mtcs/mapping/04taojin")
 
 print("Minecraft 计算机列控系统 2.0")
 print("===========================================\n")
 
-function handle(evt, ...)
-  -- print('evt:' .. (...))
+function show(...)
+    local string = ""
+
+    local args = table.pack(...)
+
+    for i = 1, args.n do
+        string = string .. tostring(args[i]) .. "\t"
+    end
+
+    return string
+end
+
+function handle(...)
+  print('evt:' .. show(...))
 end
 
 while true do
