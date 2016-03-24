0d27daf61b50a3b520428a5443ccb4b87a2e7fdc
XiNGRZ <chenxingyu92@gmail.com>
2016-03-24 23:58:53 +0800

    fix autorun

--- autorun.lua
+++ autorun.lua
@@ -2,6 +2,6 @@ local fs = require("filesystem")
 
 if (not fs.exists("/mtcs")) then
   print("Detected a new computer, installing MTCS")
-  fs.copy("/mnt/9b2/install/100_mtcs.lua", "/boot/100_mtcs.lua")
+  fs.copy("/mnt/9b2/boot/100_mtcs.lua", "/boot/100_mtcs.lua")
   print("MTCS is install, please reboot")
 end
