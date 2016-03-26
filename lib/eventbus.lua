002cd3927e424897bdd084616c4dbfc681fdaa4c
XiNGRZ <chenxingyu92@gmail.com>
2016-03-26 15:57:00 +0800

    fix keybaord

--- lib/eventbus.lua
+++ lib/eventbus.lua
@@ -24,14 +24,12 @@ function eventbus.on(address, event, listener)
   eventbus.listeners[address][event] = listener
 end
 
-eventbus.on(component.computer.address, "component_available", function(c, type)
-  if (type == "keyboard") then
-    eventbus.on(component.keyboard.address, "key_up", function(k, char, code, p)
-      if (keyboard.isAltDown() and code == keyboard.keys.r) then
-        computer.shutdown(true)
-      end
-    end)
-  end
-end)
+if (component.isAvailable('keyboard')) then
+  eventbus.on(component.keyboard.address, "key_up", function(k, char, code, p)
+    if (keyboard.isAltDown() and code == keyboard.keys.r) then
+      computer.shutdown(true)
+    end
+  end)
+end
 
 return eventbus
